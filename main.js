0443c9f49f8fc525c098f352b8fa07a3a0af8c9a
Bruno Marques <brunobmab@live.com>
2023-02-03 19:31:41 -0300

    alterações

--- main.js
+++ main.js
@@ -1,36 +1,13 @@
-const form = document.getElementById('form-submit');
-const numA = document.getElementById('Valor-A')
-const numB = document.getElementById('Valor-B')
-
-function validaAB(numA, numB) {
-    
-    if(numA < numB){
-    }
-}
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-const mensagemSucesso = `O número B=${numB.value} é maior que o número A=${numA.value}`;
-
-formEValido = validaAB(numB.value)
-    if (formEValido) {
-        const containerMensagemSucesso = document.querySelector('.sucess-messagen');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-
-        numA.value = ' ';
-        numB.value = ' ';
-        
-
-    } else {
-        numB.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
-    }
-
-})
-
-
-
-
-console.log(form);
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const fieldA = parseFloat(document.getElementById("fieldA").value);
+  const fieldB = parseFloat(document.getElementById("fieldB").value);
+  const message = document.getElementById("message");
+  if (fieldB > fieldA) {
+    message.innerHTML = "Formulário válido!";
+    message.style.color = "green";
+  } else {
+    message.innerHTML = "Formulário inválido.";
+    message.style.color = "red";
+  }
+});
